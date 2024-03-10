df10ea63be8432e490041b9152c7b1b9aa8eb9d3
zayedamin-cnits <zayedrohan.cnits@gmail.com>
2024-03-11 00:50:06 +0600

    side nav and overview page done

--- js/nav.js
+++ js/nav.js
@@ -1,43 +1,53 @@
-$(document).ready(function(){
-    $('.navbar-toggler').click(function(){
-        $('.navbar-collapse').slideToggle(300);
-    });
+// $(document).ready(function(){
+//     $('.navbar-togglers').click(function(){
+//         $('.navbar-collapses').slideToggle(300);
+//     });
     
-    smallScreenMenu();
-    let temp;
-    function resizeEnd(){
-        smallScreenMenu();
-    }
+//     smallScreenMenu();
+//     let temp;
+//     function resizeEnd(){
+//         smallScreenMenu();
+//     }
 
-    $(window).resize(function(){
-        clearTimeout(temp);
-        temp = setTimeout(resizeEnd, 100);
-        resetMenu();
-    });
-});
+//     $(window).resize(function(){
+//         clearTimeout(temp);
+//         temp = setTimeout(resizeEnd, 100);
+//         resetMenu();
+//     });
+// });
 
 
-const subMenus = $('.sub-menu');
-const menuLinks = $('.menu-link');
+// const subMenus = $('.sub-menu');
+// const menuLinks = $('.menu-link');
 
-function smallScreenMenu(){
-    if($(window).innerWidth() <= 992){
-        menuLinks.each(function(item){
-            $(this).click(function(){
-                $(this).next().slideToggle();
-            });
-        });
-    } else {
-        menuLinks.each(function(item){
-            $(this).off('click');
-        });
-    }
+// function smallScreenMenu(){
+//     if($(window).innerWidth() <= 992){
+//         menuLinks.each(function(item){
+//             $(this).click(function(){
+//                 $(this).next().slideToggle();
+//             });
+//         });
+//     } else {
+//         menuLinks.each(function(item){
+//             $(this).off('click');
+//         });
+//     }
+// }
+
+// function resetMenu(){
+//     if($(window).innerWidth() > 992){
+//         subMenus.each(function(item){
+//             $(this).css('display', 'none');
+//         });
+//     }
+// }
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "450px";
 }
+document.getElementById("mySidenav").style.width = "0";
 
-function resetMenu(){
-    if($(window).innerWidth() > 992){
-        subMenus.each(function(item){
-            $(this).css('display', 'none');
-        });
-    }
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
